f2e7627bf5b54d0427f4032e59d5b3d2a2d7a2c1
Somya Prabhakar <prabhakar.somya@gmail.com>
2023-02-14 16:42:44 -0500

    finished

--- main.js
+++ main.js
@@ -2,50 +2,62 @@
 HW 4: Somya and Dean
 */
 
-function mouseOverCircle1() {
-	let circle = document.getElementById("circle1")
-	mouseOverHandler(circle)
-}
-
-function mouseOverCircle2() {
-	let circle = document.getElementById("circle2")
-	mouseOverHandler(circle)
-}
-
-function mouseOverCircle3() {
-	let circle = document.getElementById("circle3")
-	mouseOverHandler(circle)
-}
-
-function mouseOverCircle4() {
-	let circle = document.getElementById("circle4")
-	mouseOverHandler(circle)
-}
-
 function mouseOverHandler(circle) {
 	circle.style.fill = "orange";
 }
 
-function mouseOutCircle1() {
-	let circle = document.getElementById("circle1")
-	mouseOutHandler(circle)
-}
-
-function mouseOutCircle2() {
-	let circle = document.getElementById("circle2")
-	mouseOutHandler(circle)
-}
-
-function mouseOutCircle3() {
-	let circle = document.getElementById("circle3")
-	mouseOutHandler(circle)
-}
-
-function mouseOutCircle4() {
-	let circle = document.getElementById("circle4")
-	mouseOutHandler(circle)
-}
 
 function mouseOutHandler(circle) {
 	circle.style.fill = "black";
+}
+
+
+function mouseClickHandler(circle) {
+	if (circle.style.stroke === "palevioletred") {
+		circle.style.stroke = "";
+	} else {
+		circle.style.stroke = "palevioletred";
+		circle.style.strokeWidth = 3;
+	}
+
+	// display coordinates
+	const xCoord = circle.cx.baseVal.value / 40;
+	const yCoord = (400 - circle.cy.baseVal.value) / 40;
+	const container = document.getElementById("last-point-clicked");
+	container.innerHTML = "";
+	const text = document.createTextNode(`Coordinates: (${xCoord}, ${yCoord})`);
+	container.appendChild(text)
+}
+
+
+// enter coordinate
+function enterCoordinate() {
+	// get x 
+	const xCoord = document.getElementById("x-select").value;
+	// get y
+	const yCoord = document.getElementById("y-select").value;
+	// add a point
+	const scaledX = xCoord * 40;
+	const scaledY = 400 - (yCoord * 40);
+
+	// get svg
+	const svg = document.getElementById("points");
+	// create ns container
+	const svgns = "http://www.w3.org/2000/svg";
+
+	const circle = document.createElementNS(svgns, "circle");
+	circle.setAttributeNS(null, "cx", scaledX);
+	circle.setAttributeNS(null, "cy", scaledY);
+	circle.setAttributeNS(null, "r", 10);
+	circle.setAttributeNS(null, "class", "point");
+	circle.onmouseover = () => {
+		mouseOverHandler(circle);
+	}
+	circle.onmouseout = () => {
+		mouseOutHandler(circle);
+	}
+	circle.onclick = () => {
+		mouseClickHandler(circle);
+	}
+	svg.appendChild(circle);
 }
